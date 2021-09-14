31218faf06ec8760ffaeddb40261a8eeb5f22bd8
Kümmel Christian <c.kuemmel@microstream.one>
2021-09-14 17:49:36 +0200

    zwischencommit

---
+++ src/main/java/one/microstream/controller/AuthorController.java
@@ -0,0 +1,64 @@
+package one.microstream.controller;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+import java.util.Optional;
+
+import org.apache.commons.io.IOUtils;
+
+import io.micronaut.core.io.ResourceResolver;
+import io.micronaut.core.io.scan.ClassPathResourceLoader;
+import io.micronaut.http.HttpResponse;
+import io.micronaut.http.annotation.Controller;
+import io.micronaut.http.annotation.Get;
+import one.microstream.DB;
+import one.microstream.domain.Author;
+
+
+@Controller("/authors")
+public class AuthorController
+{
+	@Get("/create")
+	public HttpResponse<String> createBooks()
+	{
+		Author author = new Author("Charlotte", "Link", "c.link@example.com");
+		
+		DB.root.getAuthors().add(author);
+		DB.storageManager.store(DB.root.getAuthors());
+		
+		return HttpResponse.ok("Author successfully created!");
+	}
+	
+	@Get("/loadImage")
+	public HttpResponse<String> loadImage()
+	{
+		ClassPathResourceLoader loader = new ResourceResolver().getLoader(ClassPathResourceLoader.class).get();
+		Optional<InputStream> isOptional = loader.getResourceAsStream("00019.MTS");
+		
+		Optional<Author> findFirst = DB.root.getAuthors().stream().findFirst();
+		
+		if(findFirst.isPresent())
+		{
+			try
+			{
+				Author author = findFirst.get();
+				author.setImage(IOUtils.toByteArray(isOptional.get()));
+				
+				DB.storageManager.store(author);
+			}
+			catch(IOException e)
+			{
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		return HttpResponse.ok("Image successfully loaded!");
+	}
+	
+	@Get
+	public List<Author> getBook()
+	{
+		return DB.root.getAuthors();
+	}
+}
---
+++ src/main/java/one/microstream/domain/Author.java
@@ -0,0 +1,58 @@
+package one.microstream.domain;
+
+public class Author
+{
+	private String	mail;
+	private String	firstname;
+	private String	lastname;
+	private byte[]	image;
+	
+	public Author(String mail, String firstname, String lastname)
+	{
+		super();
+		this.mail = mail;
+		this.firstname = firstname;
+		this.lastname = lastname;
+	}
+	
+	public String getMail()
+	{
+		return mail;
+	}
+	
+	public void setMail(String mail)
+	{
+		this.mail = mail;
+	}
+	
+	public String getFirstname()
+	{
+		return firstname;
+	}
+	
+	public void setFirstname(String firstname)
+	{
+		this.firstname = firstname;
+	}
+	
+	public String getLastname()
+	{
+		return lastname;
+	}
+	
+	public void setLastname(String lastname)
+	{
+		this.lastname = lastname;
+	}
+	
+	public byte[] getImage()
+	{
+		return image;
+	}
+	
+	public void setImage(byte[] image)
+	{
+		this.image = image;
+	}
+	
+}
