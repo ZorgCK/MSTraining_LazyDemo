19437bcdfef29a6ddc7d65e02b1bbc35756de9e2
Kümmel Christian <c.kuemmel@microstream.one>
2021-10-20 00:18:57 +0200

    zc

--- src/main/java/one/microstream/DataRoot.java
+++ src/main/java/one/microstream/DataRoot.java
@@ -4,11 +4,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 import one.microstream.domain.Author;
+import one.microstream.domain.Book;
 
 
 public class DataRoot
 {
-	private List<Author> authors = new ArrayList<Author>();
+	private List<Author>	authors	= new ArrayList<Author>();
+	private List<Book>		books	= new ArrayList<Book>();
 	
 	public List<Author> getAuthors()
 	{
@@ -18,6 +20,16 @@ public class DataRoot
 	public void setAuthors(List<Author> authors)
 	{
 		this.authors = authors;
+	}
+	
+	public List<Book> getBooks()
+	{
+		return books;
+	}
+	
+	public void setBooks(List<Book> books)
+	{
+		this.books = books;
 	}
 	
 }
---
+++ src/main/java/one/microstream/domain/Book.java
@@ -0,0 +1,55 @@
+package one.microstream.domain;
+
+public class Book
+{
+	private String	isbn;
+	private String	name;
+	private String	description;
+	private Author	author;
+	
+	public Book()
+	{
+		super();
+	}
+	
+	public String getIsbn()
+	{
+		return isbn;
+	}
+	
+	public void setIsbn(String isbn)
+	{
+		this.isbn = isbn;
+	}
+	
+	public String getName()
+	{
+		return name;
+	}
+	
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+	
+	public String getDescription()
+	{
+		return description;
+	}
+	
+	public void setDescription(String description)
+	{
+		this.description = description;
+	}
+	
+	public Author getAuthor()
+	{
+		return author;
+	}
+	
+	public void setAuthor(Author author)
+	{
+		this.author = author;
+	}
+	
+}
